c224a7575927aca982c2e2f28359a11203753d06
Justen Walker <justen.walker+github@gmail.com>
2021-09-07 21:54:51 -0400

    style: go fmt

--- netstat/main.go
+++ netstat/main.go
@@ -12,7 +12,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
-	fmt.Printf("%-4s\t%-21s\t%-21s\t%s\n","PID","LOCAL","REMOTE","STATE")
+	fmt.Printf("%-4s\t%-21s\t%-21s\t%s\n", "PID", "LOCAL", "REMOTE", "STATE")
 	for _, row := range rows {
 		remote := row.Remote.String()
 		if remote == ":0" {
